74b1a5153d7b15c2a7ced2ab5147f92a28552ddf
Matthew Habtezgi <mtarekegn143@gmail.com>
2021-01-11 12:29:49 -0500

    Update SSSP.java

--- SSSP.java
+++ SSSP.java
@@ -7,28 +7,62 @@ class Edge{
         this.b=b;
         this.w=1;
     }
+    public Edge(int a, int b, int w){
+        this.a=a;
+        this.b=b;
+        this.w=w;
+    }
 }
+
+class NodeObj{
+    int nd;
+    int cost;
+    public NodeObj(int nd, int cost){
+        this.nd=nd;
+        this.cost=cost;
+    }
+}
+
+class NodeComparator implements Comparator<NodeObj>{
+    public int compare(NodeObj one, NodeObj two){
+        if(one.cost < two.cost){
+            return 1;
+        } else if(one.cost == two.cost){
+            return 0;
+        }
+        return -1;
+    }
+}
+
 class SSSP_Runner{
-    public static LinkedList<Integer> adj[];
+    public static LinkedList<NodeObj> adj[];
     public static HashMap<Integer,Boolean> vis;
     public static HashMap<Edge,Integer> weights;
+    public static HashMap<Integer, Integer> intermediate_sp;
     public static int[] predecessor;
+    public static int[] ucs_predecessor;
+    public static PriorityQueue<NodeObj> pq;
     public SSSP_Runner(int n_nodes){
         adj = new LinkedList[n_nodes];
         weights = new HashMap<Edge,Integer>();
         vis = new HashMap<Integer,Boolean>();
+        intermediate_sp = new HashMap<Integer,Integer>();
         for(int i = 0; i < n_nodes; i++){
             adj[i] = new LinkedList();
         }
         predecessor = new int[n_nodes];
+        ucs_predecessor = new int[n_nodes];
         for(int i = 0; i < n_nodes; i++){
             predecessor[i] = -1;
+            ucs_predecessor[i] = -1;
         }
+        pq = new PriorityQueue<NodeObj>(new NodeComparator());
     }
     public void addEdge(int u, int v, int w){
-        adj[u].add(v);
-        adj[v].add(u);
-        weights.put(new Edge(u, v), w);
+        adj[u].add(new NodeObj(v,w));
+        //adj[v].add(new NodeObj(u,w));
+        //System.out.println(u + " " + v);
+        //System.out.println(v + " " + u);
     }
     //returns order of nodes in bfs
     public static ArrayList<Integer> optimal_path(int end_node){
@@ -39,9 +73,59 @@ class SSSP_Runner{
             ans.add(pt);
             pt = predecessor[pt];
         }
+        Collections.reverse(ans);
         return ans;
     }
 
+    public static ArrayList<Integer> optimal_path_ucs(int end_node){
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        int pt = ucs_predecessor[end_node];
+        ans.add(end_node);
+        while(pt != -1){
+            ans.add(pt);
+            pt = ucs_predecessor[pt];
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+
+    public static void UCS(NodeObj startNode){
+        pq.add(startNode);
+        ArrayList<Integer> seen = new ArrayList<Integer>();
+        while(pq.size() > 0){
+            NodeObj tmp = pq.poll();
+            //System.out.println(tmp.nd + " " + tmp.cost);
+            LinkedList<NodeObj> adj_list = (LinkedList<NodeObj>)(adj[tmp.nd].clone());
+            seen.add(tmp.nd);
+            int prev_nd = 0;
+            int relax_v = 100000000;
+            while(!adj_list.isEmpty()) {
+                NodeObj tp = adj_list.poll();
+                int wt = tp.cost;
+                NodeObj addend = new NodeObj(tp.nd, tmp.cost + wt);
+                relax_v = Math.min(relax_v, tmp.cost + wt);
+                int cst = tmp.cost + wt;
+                if(!intermediate_sp.containsKey(tp.nd)) {
+                    intermediate_sp.put(tp.nd, tmp.cost + wt);
+                    ucs_predecessor[tp.nd] = tmp.nd;
+                } else {
+                    intermediate_sp.put(tp.nd, Math.min(intermediate_sp.get(tp.nd), cst));
+                    if(Math.min(intermediate_sp.get(tp.nd), cst)==cst){
+                        ucs_predecessor[tp.nd] = tmp.nd;
+                    }
+                }
+                if (relax_v == tmp.cost + wt) {
+                    prev_nd = tp.nd;
+                }
+                if (!seen.contains(addend.nd)) {
+                    pq.add(addend);
+                }
+            }
+
+
+        }
+    }
+    /*
     public static ArrayList<Integer> bfs(int start_node){
         Queue<Integer> q = new LinkedList<Integer>();
         q.add(start_node);
@@ -82,13 +166,17 @@ class SSSP_Runner{
         vis.clear();
         return ret;
     }
+     */
 }
 
 public class SSSP {
     public static void main(String[] args) {
         SSSP_Runner g = new SSSP_Runner(25);
+
+        g.addEdge(0, 1, 75);
         g.addEdge(0, 3, 118);
         g.addEdge(0, 4, 140);
+
         g.addEdge(1, 0, 75);
         g.addEdge(1, 2, 71);
 
@@ -113,7 +201,7 @@ public class SSSP {
         g.addEdge(7, 11, 120);
 
         g.addEdge(8, 4, 80);
-        g.addEdge(8, 11, 146);
+        g.addEdge(8,  11, 146);
         g.addEdge(8, 10, 97);
 
 
@@ -136,7 +224,7 @@ public class SSSP {
 
         g.addEdge(14, 12, 90);
         g.addEdge(12, 14, 90);
-
+        /*
         ArrayList<Integer> one = g.bfs(0);
         ArrayList<Integer> two = g.dfs(0);
         ArrayList<Integer> three = g.optimal_path(14);
@@ -152,6 +240,20 @@ public class SSSP {
             System.out.print(i + " ");
         }
         System.out.println();
+        */
+
+        g.UCS(new NodeObj(0,0));
+        System.out.println(g.intermediate_sp.get(12));
+        g.ucs_predecessor[0] = -1;
+
+        ArrayList<Integer> wt_path =  g.optimal_path_ucs(12);
+        for(Integer i : wt_path){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+
+
     }
 
 }
